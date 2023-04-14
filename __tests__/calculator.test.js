945423d27e33c21368e3555a313796bdca2f6bed
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-14 10:38:02 -0700

    Modify 1st test to use beforeEach() to declare a user age of 60

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,9 +1,13 @@
 import Calculator from './../src/calculator.js';
 
 describe('Calculator', () => {
+  let calculator;
+  beforeEach(() => {
+    calculator = new Calculator(60);
+  });
 
+  // 1st Test
   test('should create a calculator object with default age propery', () => {
-    const calculator = new Calculator(60);
-    expect(calculator.age).toEqual(60);
+    expect(calculator.solarAge).toEqual(60);
   });
 });
