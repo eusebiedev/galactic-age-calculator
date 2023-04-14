f70dd656bee38ca1b8419e02e12576f7e2c7f85c
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-14 10:57:22 -0700

    Write 2nd test to determine users age on mercury

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -10,4 +10,9 @@ describe('Calculator', () => {
   test('should create a calculator object with default age propery', () => {
     expect(calculator.solarAge).toEqual(60);
   });
+
+  // 2nd Test
+  test('should return a users age in mercury years', () => {
+    expect(calculator.mercuryAge).toEqual(250);
+  })
 });
