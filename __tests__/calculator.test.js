342a0f5b03153377b1786a7e13cad6a97668e93a
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-14 09:58:37 -0700

    update 1st test, should create a calculator object with default age propery

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -2,9 +2,8 @@ import Calculator from './../src/calculator.js';
 
 describe('Calculator', () => {
 
-  test('should create a calculator object', () => {
-    const calculator = new Calculator();
-    expect().toEqual();
+  test('should create a calculator object with default age propery', () => {
+    const calculator = new Calculator(60);
+    expect(calculator.age).toEqual(60);
   });
-
 });
