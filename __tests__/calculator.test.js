d8597d6f36198b9ebae08aff70a02a509088bb9f
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-14 17:03:51 -0700

    Write 8th test

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -15,30 +15,35 @@ describe('Calculator', () => {
   // 2nd Test
   test('should return a users age in mercury years', () => {
     expect(age.mercuryAge()).toEqual(250);
-  })
+  });
 
   // 3rd Test
   test('should return a users age in venus years', () => {
     expect(age.venusAge()).toEqual(96.77);
-  })
+  });
 
   // 4th Test
   test('should return a users age in mars years', () => {
     expect(age.marsAge()).toEqual(31.91);
-  })
+  });
 
   // 5th Test
   test('should return a users age in jupiter years', () => {
     expect(age.jupiterAge()).toEqual(5.06);
-  })
+  });
 
   //6th Test
   test('should return how many years have passed on mercury since past birthday', () => { 
     expect(age.yearsPassedMercury()).toEqual(42);
-  })
+  });
 
   // 7th Test
   test('should return how many years have passed on venus since past birthday', () => {
     expect(age.yearsPassedVenus()).toEqual(16);
-  })
+  });
+
+  // 8th Test
+  test('should return how many years have passed on mars since past birthday', () => {
+    expect(age.yearsPassedVenus()).toEqual();
+  });
 });
