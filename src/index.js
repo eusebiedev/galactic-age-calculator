b7785d0de688804fd49d06a1a93ed69822bafd79
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-15 12:10:54 -0700

    Update index.js to remove unnecessary call for UI

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Calculator from './calculator.js';
-
-
-function handleFormSubmission(event) {
-  event.preventDefault();
-  document.querySelector('#').innerText = null;
-  const calculator = new Calculator();
-}
-
-window.addEventListener("load", function() {
-  document.querySelector("#").addEventListener("submit", handleFormSubmission);
-});
